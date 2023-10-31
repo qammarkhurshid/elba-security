3b604a0362253329a3b51c078b33d16207682a39
Loïc Weinhard <lweinhard.pro@gmail.com>
2023-10-31 16:41:10 +0100

    feat: init Microsoft TPA with Hono

--- apps/microsoft-tpa/src/app/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { createElysia } from 'src/util/elysia';
-import { routes as authRoutes } from './auth';
-import { routes as scanRoutes } from './scan';
-
-export const routes = createElysia({ prefix: '/api' })
-  .use(authRoutes)
-  .use(scanRoutes);
