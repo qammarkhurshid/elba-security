05a6abea87d0c481ace3eaca79051fa6a9b636b9
Alexandre Nicolas <alexandre@elba.security>
2023-11-06 15:06:46 +0100

    feat(github): add elba repository

---
+++ apps/github/repositories/elba/elba.repository.ts
@@ -0,0 +1,19 @@
+import { env } from '../../env';
+import { RequestSender } from './RequestSender';
+import { ThirdPartyApps } from './resources/third-party-apps/ThirdPartyApps';
+import { Users } from './resources/users/Users';
+
+export class ElbaRepository {
+  readonly users: Users;
+  readonly thridPartyApps: ThirdPartyApps;
+
+  constructor(organisationId: string) {
+    const requestSender = new RequestSender({
+      organisationId,
+      sourceId: env.ELBA_SOURCE_ID,
+      baseUrl: env.ELBA_API_BASE_URL,
+    });
+    this.users = new Users(requestSender);
+    this.thridPartyApps = new ThirdPartyApps(requestSender);
+  }
+}
---
+++ apps/github/repositories/elba/resources/users/Users.ts
@@ -0,0 +1,20 @@
+import { ElbaResource } from '../ElbaResource';
+import type { User } from './types';
+
+export class Users extends ElbaResource {
+  async updateUsers(users: User[]) {
+    const response = await this.requestSender.request('users', {
+      method: 'POST',
+      data: { users },
+    });
+    return response.json();
+  }
+
+  async deleteUsers(lastSyncedBefore: Date) {
+    const response = await this.requestSender.request('users', {
+      method: 'DELETE',
+      data: { lastSyncedBefore },
+    });
+    return response.json();
+  }
+}
