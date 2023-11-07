298bc6af29ba8692baee305a28a7781fe0990c22
Alexandre Nicolas <alexandre@elba.security>
2023-11-07 16:23:22 +0100

    chore(github): add docker postgres & drizzle migration

--- apps/github/env.ts
+++ apps/github/env.ts
@@ -2,10 +2,26 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_ELBA_GITHUB_INSTALL_URL: z.string().url(),
+  },
   server: {
     ELBA_API_BASE_URL: z.string().url(),
     ELBA_SOURCE_ID: z.string(),
+    POSTGRES_HOST: z.string(),
+    POSTGRES_PORT: z.string().transform(Number).pipe(z.number().int()),
+    POSTGRES_USERNAME: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DATABASE: z.string(),
+    GITHUB_APP_ID: z.string(),
+    GITHUB_PRIVATE_KEY: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
   },
-  // @ts-expect-error -- why ?
-  runtimeEnv: process.env,
+  // @ts-expect-error process.env is not typed according to .env
+  runtimeEnv: {
+    ...process.env,
+    // client env need to be specified explicitly
+    NEXT_PUBLIC_ELBA_GITHUB_INSTALL_URL: process.env.NEXT_PUBLIC_ELBA_GITHUB_INSTALL_URL,
+  },
 });
---
+++ apps/github/scripts/migrate.js
@@ -0,0 +1,19 @@
+const { drizzle } = require('drizzle-orm/postgres-js');
+const { migrate } = require('drizzle-orm/postgres-js/migrator');
+const dotenv = require('dotenv');
+const postgres = require('postgres');
+
+// setup process.env from .env file
+dotenv.config();
+
+const sql = postgres({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  db: process.env.POSTGRES_DATABASE,
+});
+
+const db = drizzle(sql);
+
+migrate(db, { migrationsFolder: './drizzle' }).finally(sql.end);
