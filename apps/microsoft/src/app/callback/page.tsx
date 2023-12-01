ff1ce528bd242b50857b4be93b79884ad2e9b9f0
Loïc Weinhard <lweinhard.pro@gmail.com>
2023-12-01 16:18:08 +0100

    fix(callback): change error handler

--- apps/microsoft/src/app/callback/page.tsx
+++ apps/microsoft/src/app/callback/page.tsx
@@ -1,6 +1,4 @@
 import { cookies } from 'next/headers';
-import { RedirectType, redirect } from 'next/navigation';
-import { isRedirectError } from 'next/dist/client/components/redirect';
 import { handleMicrosoftAuthCallback } from '@/repositories/microsoft/auth';
 
 type CallbackPageProps = {
@@ -13,18 +11,14 @@ export default async function CallbackPage({ searchParams }: CallbackPageProps) 
   const organizationId = cookies().get('organizationId')?.value;
 
   try {
-    await handleMicrosoftAuthCallback({
+    const result = await handleMicrosoftAuthCallback({
       isAdminConsentGiven,
       tenantId,
       elbaOrganizationId: organizationId,
     });
     // TODO - replace url by elba install success
-    redirect('https://foo.bar', RedirectType.replace);
+    return result;
   } catch (error) {
-    if (isRedirectError(error)) {
-      throw error;
-    }
-    // TODO - replace url by elba install error
-    redirect('https://foo.bar?error=true', RedirectType.replace);
+    throw new Error('Something wrong happened', { cause: error });
   }
 }
