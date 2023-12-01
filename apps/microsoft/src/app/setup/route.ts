4836b83d5d7a7f07e1b4abd73cddab6405f6924e
Loïc Weinhard <lweinhard.pro@gmail.com>
2023-12-01 16:02:15 +0100

    feat: add setup route

---
+++ apps/microsoft/src/app/setup/route.ts
@@ -0,0 +1,18 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { type NextRequest } from 'next/server';
+import { formatMicrosoftConsentUrl } from '@/common/microsoft';
+
+export const dynamic = 'force-dynamic';
+
+export function GET(request: NextRequest) {
+  const organisationId = request.nextUrl.searchParams.get('organisation_id');
+
+  if (!organisationId) {
+    // TODO - replace url by elba install error
+    redirect('https://foo.bar?error=true');
+  }
+
+  cookies().set('organisationId', organisationId);
+  redirect(formatMicrosoftConsentUrl());
+}
