32f78f48f639c505dc0263c2ae143e3c65e4dc22
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2023-12-05 09:29:53 +0100

    chore: use edge runtime for inngest

--- apps/slack/src/app/api/inngest/route.ts
+++ apps/slack/src/app/api/inngest/route.ts
@@ -2,7 +2,11 @@ import { serve } from 'inngest/next';
 import { inngest } from '@/inngest/client';
 import { inngestFunctions } from '@/inngest/functions';
 
+export const runtime = 'edge';
+export const preferredRegion = 'fra1';
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: inngestFunctions,
+  streaming: 'allow',
 });
