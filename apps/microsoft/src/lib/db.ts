e97464e377a5d40597951f885555c075d14d88ba
Loïc Weinhard <lweinhard.pro@gmail.com>
2023-12-06 19:18:10 +0100

    fix: add ssl to database client

--- apps/microsoft/src/lib/db.ts
+++ apps/microsoft/src/lib/db.ts
@@ -2,6 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '@/common/env';
 
-const queryClient = postgres(env.POSTGRES_URL);
-
+const queryClient = postgres(
+  env.VERCEL_ENV && env.VERCEL_ENV !== 'development'
+    ? `${env.POSTGRES_URL}?sslmode=require`
+    : env.POSTGRES_URL
+);
 export const db = drizzle(queryClient);
