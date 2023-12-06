3a7f43717417298cbf7fabb3ed1cd9fd86366713
Alexandre Nicolas <alexandre@elba.security>
2023-12-06 16:33:33 +0100

    chore(github): fix vercel db connection issue

--- apps/github/src/database/client.ts
+++ apps/github/src/database/client.ts
@@ -2,6 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '@/env';
 
-const queryClient = postgres(env.POSTGRES_URL);
+const queryClient = postgres(
+  env.VERCEL_ENV && env.VERCEL_ENV !== 'development'
+    ? `${env.POSTGRES_URL}?sslmode=require`
+    : env.POSTGRES_URL
+);
 
 export const db = drizzle(queryClient);
